ae8bd31f442a016e1a642af48547f3b538a28088
arifyogaw <septiandolo@gmail.com>
2023-05-22 01:15:03 +0700

    footer text justify

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,10 +6,10 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = ({ className }) => {
   return (
-  <footer className={`bg-main py-4 text-center ${className}`}>
+  <footer className={`bg-main py-4 text-justify ${className}`}>
   <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
     <div className="lg:flex lg:items-start lg:gap-8">
-      <div className="mt-8 grid grid-cols-2 gap-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16">
+      <div className="mt-8 grid grid-cols-3 gap-8 lg:mt-0 lg:grid-cols-3 lg:gap-y-16">
         <div className="col-span-2 sm:col-span-1 md:col-span-1 lg:col-span-1 xl:col-span-1">
           <p className="font-large text-gray-900">Menu</p>
           <nav aria-label="Footer Navigation - Menu" className="mt-6">
@@ -24,7 +24,7 @@ const Footer: React.FC<FooterProps> = ({ className }) => {
         </div>
         <div className="col-span-2 sm:col-span-1 md:col-span-1 lg:col-span-1 xl:col-span-1">
           <p className="font-large text-gray-900">Kategori</p>
-          <nav aria-label="Footer Navigation - Company" className="mt-6">
+          <nav aria-label="Footer Navigation - Kategori" className="mt-6">
             <ul className="space-y-4 text-sm">
               <li>
                 <a href="#" className="text-gray-700 transition hover:opacity-75">
@@ -50,7 +50,7 @@ const Footer: React.FC<FooterProps> = ({ className }) => {
           </nav>
         </div>
         <div className="col-span-2 sm:col-span-1 md:col-span-1 lg:col-span-1 xl:col-span-1">
-          <p className="font-large text-gray-900">Kontak</p>
+          <p className="font-large text-gray-900 ml-auto">Kontak</p>
         </div>
       </div>
     </div>
