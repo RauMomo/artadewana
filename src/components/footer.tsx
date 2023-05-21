8a395a6fde3bd9373f74d51000f19f901c118de1
arifyogaw <septiandolo@gmail.com>
2023-05-22 00:43:36 +0700

    footer responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,56 +6,57 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = ({ className }) => {
   return (
-    <footer className={`bg-main py-4 text-center ${className}`}>
-      <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-        <div className="lg:flex lg:items-start lg:gap-8">
-          <div className="mt-8 grid grid-cols-2 gap-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16">
-            <div className="col-span-2 sm:col-span-1">
-              <p className="font-medium text-gray-900">Menu</p>
-              <nav aria-label="Footer Navigation - Services" className="mt-6">
-                <ul className="space-y-4 text-sm">
-                  <li>
-                    <a href="#" className="text-gray-700 transition hover:opacity-75">
-                      Tentang Kami
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-            <div className="col-span-2 sm:col-span-1">
-              <p className="font-medium text-gray-900">Kategori</p>
-              <nav aria-label="Footer Navigation - Company" className="mt-6">
-                <ul className="space-y-4 text-sm">
-                  <li>
-                    <a href="#" className="text-gray-700 transition hover:opacity-75">
-                      Gerabak
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-gray-700 transition hover:opacity-75">
-                      Keramik
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-gray-700 transition hover:opacity-75">
-                      Porcelin
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-gray-700 transition hover:opacity-75">
-                      Bata & Genteng
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-            <div className="col-span-2 sm:col-span-1 flex justify-end">
-              <p className="font-medium text-gray-900">Kontak</p>
-            </div>
-          </div>
+  <footer className={`bg-main py-4 text-center ${className}`}>
+  <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+    <div className="lg:flex lg:items-start lg:gap-8">
+      <div className="mt-8 grid grid-cols-2 gap-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16">
+        <div className="col-span-2 sm:col-span-1 md:col-span-1 lg:col-span-1 xl:col-span-1">
+          <p className="font-large text-gray-900">Menu</p>
+          <nav aria-label="Footer Navigation - Menu" className="mt-6">
+            <ul className="space-y-4 text-sm">
+              <li>
+                <a href="#" className="text-gray-700 transition hover:opacity-75">
+                  Tentang Kami
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="col-span-2 sm:col-span-1 md:col-span-1 lg:col-span-1 xl:col-span-1">
+          <p className="font-large text-gray-900">Kategori</p>
+          <nav aria-label="Footer Navigation - Company" className="mt-6">
+            <ul className="space-y-4 text-sm">
+              <li>
+                <a href="#" className="text-gray-700 transition hover:opacity-75">
+                  Gerabak
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-gray-700 transition hover:opacity-75">
+                  Keramik
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-gray-700 transition hover:opacity-75">
+                  Porcelin
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-gray-700 transition hover:opacity-75">
+                  Bata & Genteng
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="col-span-2 sm:col-span-1 md:col-span-1 lg:col-span-1 xl:col-span-1">
+          <p className="font-large text-gray-900">Kontak</p>
         </div>
       </div>
-    </footer>
+    </div>
+  </div>
+</footer>
+
   );
 };
 
