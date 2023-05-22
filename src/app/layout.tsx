6821e23f003a1a35e260d89bbcaf0abefcf1e08e
muhammadrizqiko <muhammadrizqiko@atmatech.net>
2023-05-22 17:50:26 +0700

    edit layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
+import Header from '@/components/header'
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +12,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <Header/>
+      <body>{children}</body>
+    </>
   )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <div className="flex items-center lg mx-auto text-2xl font-bold place-content-center py-10">Artadewana</div>
+    </div>
+  );
+}
