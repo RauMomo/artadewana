5c79e244d21c4f3c334d226e70e47970794f004d
muhammadrizqiko <muhammadrizqiko@atmatech.net>
2023-06-21 23:38:01 +0700

    hamburger menu completed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import { categoryLists } from "@/hooks/product";
+import { categoryLists } from '@/hooks/product';
 import { useState } from 'react';
-import HeaderMenu from "./HeaderMenu";
-import PopupMenu from "./PopupMenu";
+import HeaderMenu from './HeaderMenu';
+import PopupMenu from './PopupMenu';
 
 export default function Header() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -22,7 +22,7 @@ export default function Header() {
   const updateMenu = () => {
         if(!isMenuClicked) {
             setBurgerClass("burger-bar clicked")
-            setMenuClass("menu visible")
+            setMenuClass("menu visible bg-main")
         }
         else {
             setBurgerClass("burger-bar unclicked")
@@ -32,7 +32,7 @@ export default function Header() {
     }
 
   return (
-    <div className="bg-main min-w-fit max-w-screen-xl w-screen flex flex-nowrap flex-col md:w-auto lg:max-w-fit container flex-shrink-0 lg:min-w-full">
+    <div className="bg-main min-w-fit max-w-screen-xl w-full flex flex-nowrap flex-col md:w-auto lg:max-w-fit container flex-shrink-0 lg:min-w-full z-10">
       <div className="flex flex-row justify-between items-center px-4 lg:px-0 lg:justify-center">
         <div className="flex flex-col items-start justify-between">
           <div className="text-2xl font-bold pt-8 text-black lg:ml-auto lg:mr-auto flex-nowrap">
@@ -40,14 +40,52 @@ export default function Header() {
             <div className="inline-flex lg:hidden" />
           </div>
         </div>
-        <button data-collapse-toggle="navbar-hamburger" type="button" className="lg:hidden p-2 text-sm text-gray-500 rounded-lg focus:outline-none focus:ring-gray-200 dark:text-gray-400" aria-controls="navbar-hamburger" aria-expanded="false">
+        <button data-collapse-toggle="navbar-hamburger" type="button" onClick={updateMenu} className="lg:hidden p-2 text-sm text-gray-500 rounded-lg focus:outline-none focus:ring-gray-200 dark:text-gray-400" aria-controls="navbar-hamburger" aria-expanded="false">
           <span className="sr-only">Open main menu</span>
           <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
         </button>
       </div>
       <div className="h-1 min-w-screen bg-white border-white" />
+      <div className={menuClass}>
+        <div className='min-w-fit max-w-screen-xl w-full flex flex-nowrap flex-col'>
+        <div className="flex flex-row justify-between items-center px-4 lg:px-0 lg:justify-center">
+        <div className="flex flex-col items-start justify-between">
+          <div className="text-2xl font-bold pt-8 text-black lg:ml-auto lg:mr-auto flex-nowrap">
+            <div className="text-black text-2xl font-bold lg:pb-8 text-center lg:ml-auto">Artadewana</div>
+            <div className="inline-flex lg:hidden" />
+          </div>
+        </div>
+        <button data-collapse-toggle="navbar-hamburger" type="button" onClick={updateMenu} className="lg:hidden p-2 text-sm text-gray-500 rounded-lg focus:outline-none focus:ring-gray-200 dark:text-gray-400" aria-controls="navbar-hamburger" aria-expanded="false">
+          <span className="sr-only">Open main menu</span>
+          <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+        </button>
+        </div>
+        <ul className="block font-medium mt-4 rounded-lg align-middle text-white">
+        {categoryLists.map((category, index) => {
+          return (
+              <li className="h-20">
+                  <div className="align-middle px-4 text-black">{category}</div>
+              </li>
+          )
+        })}
+          <li className="h-20">
+              <div className="align-middle px-4 text-black" onClick={() => {
+                openPopup()
+              }}>Hubungi Kami</div>
+          </li>
+            <li className="h-20">
+              <div className="align-middle px-4 text-black" onClick={() => {
+                openPopup()
+              }}>Tentang Kami</div>
+              {isPopupOpen && (
+                <PopupMenu onClick={closePopup} />
+              )}
+        </li>
+        </ul>
+        </div>
+      </div>
       <div className="items-center justify-center flex-row mr-auto ml-auto">
-        <ul className="flex flex-row font-medium my-2 rounded-lg bgt-main" key={'ul'}>
+        <ul className="nav-bar" key={'ul'}>
         {categoryLists.map((category, index) => {
           return (
             <>
